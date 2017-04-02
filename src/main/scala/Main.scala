a0898902b590dd608d3210f8d1ed979bcf242ab1
Your Name <benko.balog@gmail.com>
2017-04-02 13:15:42 +0200

    Making types more exact, minor fixes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,7 +9,7 @@ object Main {
     Key.exec.maxWarmupRuns -> 80,
     Key.exec.benchRuns -> 120,
     Key.verbose -> false
-  ) withWarmer(new Warmer.Default)
+  ).withWarmer(new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
     val limits: List[Double] = List(1e3, 2e3, 5e3,
@@ -17,9 +17,9 @@ object Main {
                                     1e5, 2e5, 5e5,
                                     1e6)
     println("======= Measure with 2 operations =======")
-    limits.map(_.toInt).foreach(measureFns(mapFilter2op(_), collect2op(_), loop2op(_)))
+    limits.map(_.toInt).foreach(measureFns(mapFilter2op, collect2op, loop2op))
     println("======= Measure with 3 operations =======")
-    limits.map(_.toInt).foreach(measureFns(mapFilter3op(_), collect3op(_), loop3op(_)))
+    limits.map(_.toInt).foreach(measureFns(mapFilter3op, collect3op, loop3op))
   }
 
   def mapFilter2op(list: List[Int]) = {
@@ -74,8 +74,8 @@ object Main {
   }
 
   def measureFns(
-                  mapFilterOp: List[Int] => Unit,
-                  collectOp: List[Int] => Unit,
+                  mapFilterOp: List[Int] => List[_],
+                  collectOp: List[Int] => List[_],
                   loopOp: List[Int] => Unit
                 )(upperLimit: Int): Unit = {
     val list = (0 to upperLimit).toList
@@ -96,10 +96,10 @@ object Main {
     println(s"   collect time: $collectTime on $upperLimit elements")
     println(s"      loop time: $loopTime on $upperLimit elements")
     val mfPerLoop = mapFilterTime.value - loopTime.value
-    val mfPerLooptimes = mapFilterTime.value / loopTime.value
+    val mfPerLoopTimes = mapFilterTime.value / loopTime.value
     val mfPerCollectTimes = mapFilterTime.value / collectTime.value
     val collectPerLoop = collectTime.value / loopTime.value
-    println(s"map filter /    loop: $mfPerLooptimes")
+    println(s"map filter /    loop: $mfPerLoopTimes")
     println(s"map filter / collect: $mfPerCollectTimes")
     println(s"collect    /    loop: $collectPerLoop")
     println(s"loop       / collect: ${1.0 / collectPerLoop}")
